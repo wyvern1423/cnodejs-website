83b93acbb706d8f9bcb24788849a92ebaf0a227e
wyvern1423 <xiaoxun1208@126.com>
2020-04-14 18:30:26 +0800

    deploy

---
+++ precache-manifest.97ad997be991e6147d6dae63f754a7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51746a1c03a0dd258fd",
+    "url": "/cnodejs-website/css/app.b686341f.css"
+  },
+  {
+    "revision": "86f6a7c2f6cf9844d8a9f5d088d87406",
+    "url": "/cnodejs-website/img/logo.86f6a7c2.svg"
+  },
+  {
+    "revision": "b56fac4260bbca128148ec7cd8df645d",
+    "url": "/cnodejs-website/index.html"
+  },
+  {
+    "revision": "d51746a1c03a0dd258fd",
+    "url": "/cnodejs-website/js/app.8b756c1b.js"
+  },
+  {
+    "revision": "88e95b85f17e59f8fc7f",
+    "url": "/cnodejs-website/js/chunk-vendors.cd32d23e.js"
+  },
+  {
+    "revision": "53656bdc78c24067349596cab3d4c767",
+    "url": "/cnodejs-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cnodejs-website/robots.txt"
+  }
+]);
